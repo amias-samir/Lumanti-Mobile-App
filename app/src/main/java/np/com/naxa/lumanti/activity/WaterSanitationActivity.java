a0ec56a192f0981134f2b8de5d5fa236da50754b
Samir Dangal <Samir Dangal>
2017-06-23 13:03:43 +0545

    water sanitation done

--- app/src/main/java/np/com/naxa/lumanti/activity/WaterSanitationActivity.java
+++ app/src/main/java/np/com/naxa/lumanti/activity/WaterSanitationActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnItemSelected;
 import np.com.naxa.lumanti.R;
 import np.com.naxa.lumanti.model.GeneralFormModel;
 
@@ -46,11 +48,33 @@ public class WaterSanitationActivity extends AppCompatActivity {
         generalFormModel = (GeneralFormModel) getIntent().getSerializableExtra("generalFormModel");
         Toast.makeText(this, ""+ generalFormModel.getG1(), Toast.LENGTH_SHORT).show();
 
+        tvOtherDrinkingWaterSource.setVisibility(View.INVISIBLE);
+
     }
 
     @OnClick(R.id.water_sanitation_next)
     public void NextPage() {
+
+        generalFormModel.setE1(spinnerDrinkingWaterSource.getSelectedItem().toString());
+        generalFormModel.setE1_a(tvOtherDrinkingWaterSource.getText().toString());
+        generalFormModel.setE2(spinnerToiletType.getSelectedItem().toString());
+
         Intent intent = new Intent(WaterSanitationActivity.this, CapacityBuildingActivity.class);
+        intent.putExtra("generalFormModel", generalFormModel);
         startActivity(intent);
+    }
+
+
+    @OnItemSelected(R.id.water_sanitation_drinking_water_source)
+    public void spinnerOtherWaterSelected(Spinner spinner, int position) {
+        // code here
+        int id = position ;
+        String selected_item = spinnerDrinkingWaterSource.getSelectedItem().toString();
+        if (selected_item.equals("Others")){
+            tvOtherDrinkingWaterSource.setVisibility(View.VISIBLE);
+        }
+        else {
+            tvOtherDrinkingWaterSource.setVisibility(View.INVISIBLE);
+        }
     }
 }
